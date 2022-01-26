f13dbc15133d4ce6eb2f78edb3e8df48062f5cb7
Breno Delgado <brenodelgado.dev@gmail.com>
2022-01-26 10:45:49 -0300

    Primeiro Commit

---
+++ src/main/java/com/pfi/pfisoftware/PfiSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.pfi.pfisoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfiSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfiSoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pfi/pfisoftware/PfiSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfi.pfisoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfiSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
